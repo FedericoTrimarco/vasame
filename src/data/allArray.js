9605465391cea2aa61c7e1785336377128bb7658
FedericoTrimarco <trimarcofederico98@gmail.com>
2022-01-06 19:40:14 +0100

    add allArray

---
+++ src/data/allArray.js
@@ -0,0 +1,57 @@
+export const vases = [
+    {
+        imgSrc: '../assets/genni.jpeg',
+        title: 'GENNI',
+        info: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur nostrum repudiandae nihil blanditiis expedita? Incidunt tempore consequatur eum quaerat fugiat impedit fugit quasi, excepturi fuga veritatis dignissimos porro sequi modi.',
+    },
+    {
+        imgSrc: '../assets/ciro.jpeg',
+        title: 'CIRO',
+        info: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur nostrum repudiandae nihil blanditiis expedita? Incidunt tempore consequatur eum quaerat fugiat impedit fugit quasi, excepturi fuga veritatis dignissimos porro sequi modi.',
+    },
+    {
+        imgSrc: '../assets/concetta.jpeg',
+        title: 'GENNI',
+        info: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur nostrum repudiandae nihil blanditiis expedita? Incidunt tempore consequatur eum quaerat fugiat impedit fugit quasi, excepturi fuga veritatis dignissimos porro sequi modi.',
+    },
+    {
+        imgSrc: '../assets/enzo.jpeg',
+        title: 'ENZO',
+        info: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur nostrum repudiandae nihil blanditiis expedita? Incidunt tempore consequatur eum quaerat fugiat impedit fugit quasi, excepturi fuga veritatis dignissimos porro sequi modi.',
+    },
+    {
+        imgSrc: '../assets/imma.jpeg',
+        title: 'IMMA',
+        info: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur nostrum repudiandae nihil blanditiis expedita? Incidunt tempore consequatur eum quaerat fugiat impedit fugit quasi, excepturi fuga veritatis dignissimos porro sequi modi.',
+    },
+    {
+        imgSrc: '../assets/lello.jpeg',
+        title: 'LELLO',
+        info: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur nostrum repudiandae nihil blanditiis expedita? Incidunt tempore consequatur eum quaerat fugiat impedit fugit quasi, excepturi fuga veritatis dignissimos porro sequi modi.',
+    },
+    {
+        imgSrc: '../assets/lorena.jpeg',
+        title: 'LORENA',
+        info: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur nostrum repudiandae nihil blanditiis expedita? Incidunt tempore consequatur eum quaerat fugiat impedit fugit quasi, excepturi fuga veritatis dignissimos porro sequi modi.',
+    },
+    {
+        imgSrc: '../assets/nunzia.jpeg',
+        title: 'NUNZIA',
+        info: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur nostrum repudiandae nihil blanditiis expedita? Incidunt tempore consequatur eum quaerat fugiat impedit fugit quasi, excepturi fuga veritatis dignissimos porro sequi modi.',
+    },
+    {
+        imgSrc: '../assets/pina.jpeg',
+        title: 'PINA',
+        info: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur nostrum repudiandae nihil blanditiis expedita? Incidunt tempore consequatur eum quaerat fugiat impedit fugit quasi, excepturi fuga veritatis dignissimos porro sequi modi.',
+    },
+    {
+        imgSrc: '../assets/rosaria.jpeg',
+        title: 'ROSARIA',
+        info: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur nostrum repudiandae nihil blanditiis expedita? Incidunt tempore consequatur eum quaerat fugiat impedit fugit quasi, excepturi fuga veritatis dignissimos porro sequi modi.',
+    },
+    {
+        imgSrc: '../assets/titta.jpeg',
+        title: 'TITTA',
+        info: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur nostrum repudiandae nihil blanditiis expedita? Incidunt tempore consequatur eum quaerat fugiat impedit fugit quasi, excepturi fuga veritatis dignissimos porro sequi modi.',
+    },
+]
