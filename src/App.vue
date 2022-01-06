8137afc16f54d27b98647822b80336e5ee9d350b
FedericoTrimarco <trimarcofederico98@gmail.com>
2022-01-06 12:43:06 +0100

    add header structure

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,23 @@
 <template>
   <div id="app">
-    
+    <Header />
   </div>
 </template>
 
 <script>
-
+import Header from '@/components/Header.vue'
 
 export default {
   name: 'App',
   components: {
-   
+   Header,
   }
 }
 </script>
 
 <style lang="scss">
-
+@import './style/variables.scss';
+#app{
+  background-color: $firstColor;
+}
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+    <header class="d-flex justify-content-around align-items-center">
+        <div class="img-logo">
+            <img src="../assets/logo-vasame-transparent.png" alt="">
+        </div>
+
+        <nav class="info">
+            <ul class="list-unstyled d-flex fs-3 ">
+                <li>about</li>
+                <li class="mx-4">shop</li>
+                <li>our items</li>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+<script>
+export default {
+    name:'Header'
+}
+</script>
+
+<style scoped lang="scss">
+@import '../style/variables.scss';
+    header{
+        background-color: $firstColor;
+        img{
+            height: 180px;
+        }
+    }
+</style>
